cc69757428cf336a42e55a4c49de63e8921951a1
Youjin Shin <youjinshin@Youjins-MacBook-Pro.local>
2024-01-18 17:13:50 +0900

    await function added

--- src/starter.js
+++ src/starter.js
@@ -6,17 +6,3 @@ import "./viz.css";
 // svg
 const svg = d3.select("#svg-container").append("svg").attr("id", "svg");
 const g = svg.append("g"); // group
-
-// parsing & formatting
-
-// scale
-
-// axis
-
-// line
-
-// svg elements
-
-////////////////////////////////////////////////////////////////////
-////////////////////////////  Load CSV  ////////////////////////////
-//  data (d3.csv)
